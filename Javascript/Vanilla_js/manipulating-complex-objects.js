eb8b3640df7b789bb50b0cd52c830d8f65573c05
kingsmandralph <ekeneumegakwe@gmail.com>
2022-07-08 16:38:20 +0100

    JavaScript fundamentals

---
+++ Javascript/Vanilla_js/manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "My Gamer",
+    "title": "Juice wrld",
+    "release_year": 2022,
+    "formats": [
+      "me",
+      "you",
+      "not us"
+    ],
+  }
+];
